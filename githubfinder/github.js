7afc3865a49e46c5cc03189c79a1c24118a7e272
Juan Maurente <juanmaurente@gmail.com>
2021-11-20 19:33:37 +1100

    upload folder

---
+++ githubfinder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor(){
+    this.client_id = '58017d8f772cda0905fe';
+    this.client_secret = '0007ce4c43042fb91fff48b157b42759d123db4c';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
